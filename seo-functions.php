54ed19ce9b39ece5b5e15aaf4f1bb2cec635271a
Jonathan Engstrom <jonathan@oldbutchershop.co.uk>
2014-05-23 10:17:48 +0100

    Improved Meta

---
+++ seo-functions.php
@@ -0,0 +1,14 @@
+<?php
+  
+  /*
+  
+  Include this file into your themes 'functions.php' file
+  to install this part of the module:
+  
+  require_once('path-to/seo-functions.php');
+  
+  */  
+  
+  
+  require_once('seo-fields.php');     // include custom fields
+  require_once('seo-sitemap.php');    // include sitemap
--- seo-meta.php
+++ seo-meta.php
@@ -1,29 +1,70 @@
-<?php if ( is_front_page() ) { ?>
- 
-  <title><?php bloginfo('name'); ?></title>
-  <meta name="description" content="<?php bloginfo('description'); ?>">
+<?php
   
-<?php } elseif ( is_page() || is_single() ) { 
+  /*
   
-  $title = get_field( "meta_title" );
-  $description = get_field( "meta_description" );
+  This file writes our <title> and <description> meta tags,
+  and also adds a robots nofollow instruction for pages that
+  the user does not want to appear in search engines.
   
-  if( $title ) { ?>
+  Include this file into your themes 'header.php' file inside the <head>
+  to install this part of the module.
+  
+  */ 
+  
+  
+  /* if front page == posts page */
+  if ( is_front_page() && is_home() ) {                                                  // if you do NOT have a static front page
     
-    <title><?php the_field('meta_title'); ?></title>
+    echo "<title>" . get_bloginfo('name') . "</title>";                                  // echo the blog title (in settings)
+    echo "<meta name='description' content='" .  get_bloginfo('description') . "'>";     // echo the blog description (in settings)
     
-  <?php } else { ?>
     
-    <title><?php the_title(); ?></title>
+  /* pages, posts & static front page */    
+  } elseif ( is_page() || is_single() ) {                                                // if you DO have astatic front page, plus other posts & pages
     
-  <?php } if( $description ) { ?>
+    $title = get_field( 'meta_title', $page_id );                                        // set title variable as our custom input
+    $description = get_field( 'meta_description', $page_id );                            // set description variable as our custom textarea
     
-    <meta name="description" content="<?php the_field('meta_description'); ?>">
+    if ( $title ) {                                                                      // if our custom input has been used
+      echo "<title>" . $title . "</title>";                                              // pull in the title from there
+    } else {
+      echo "<title>" . get_the_title() . "</title>";                                     // use the post/page title as a fallback
+    }
+    
+    if ( $description ) {                                                                // if our custom textarea has been used
+      echo "<meta name='description' content='" .  $description . "'>";                  // pull it into our description (no fallback needed)
+    }
+    
+  /* posts page */  
+  } elseif ( is_home() ) {                                                               // if your home page IS static, this is for the posts page
+    
+    $page_id = get_option( 'page_for_posts' );                                           // set the page id to = the posts page
+    
+    if ( $title ) {                                                                      // if our custom input has been used
+      echo "<title>" . $title . "</title>";                                              // pull in the title from there
+    } else {
+      echo "<title>" . get_bloginfo('name') . " - Blog</title>";                         // use the blog name as a fallback
+    }
+    
+    if ( $description ) {                                                                // if our custom textarea has been used
+      echo "<meta name='description' content='" .  get_bloginfo('name') . "'>";          // pull it into our description (no fallback needed)
+    }
+    
+    
+  /* all other page types (eg, 404, search, archives) */  
+  } else {
+    
+    echo "<title>";                                                                      // open the <title> tag
+    echo get_bloginfo('name');                                                           // get the blog title first
+    echo wp_title('|', true, 'left');                                                    // then call the wp_title function
+    echo "</title>";                                                                     // close the <title>
+    
+  }
   
-<?php } } else { ?>
   
-  <title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
+  /* if user chooses to hide the page from search engines */  
+  if ( is_search() || get_field('private_page') ) {
+    echo '<meta name="robots" content="noindex, nofollow" />';                           // echo the robots nofollow meta tag
+  } 
   
-<?php } ?>
-
-<?php if ( is_search() || get_field('private_page') ) { echo '<meta name="robots" content="noindex, nofollow" />'; } ?>
+?>
