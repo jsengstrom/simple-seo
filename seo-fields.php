726e31bec60ea35c55c88bd53f87ddebdf12bfc3
Jonathan Engstrom <jonathan@oldbutchershop.co.uk>
2014-05-23 10:16:39 +0100

    Re-built Sitemap Function
    Sitemap function will now write an extra <url> entry for the front page
    if that is equal to the posts page. Also added commenting.

--- seo-fields.php
+++ seo-fields.php
@@ -1,6 +1,13 @@
-<?php 
+<?php
   
-  require_once('seo-sitemap.php');
+  /*
+  
+  This file generates our custom fields, as exported from the
+  Advanced Custom Fields plugin. There is no need do anything
+  with this. It is included within 'seo-functions.php'
+  
+  */ 
+  
   
   if( function_exists('register_field_group') ):
   
@@ -8,6 +15,7 @@
     'key' => 'group_533d8805cdba7',
     'title' => 'Simple SEO',
     'fields' => array (
+      /* set the General tab */
       array (
         'key' => 'field_5342821f6d723',
         'label' => 'General',
@@ -18,6 +26,7 @@
         'required' => 0,
         'conditional_logic' => 0,
       ),
+      /* set our <title> field */
       array (
         'key' => 'field_533d880cc87b7',
         'label' => 'Meta Title',
@@ -36,6 +45,7 @@
         'readonly' => 0,
         'disabled' => 0,
       ),
+      /* set our <description> field */
       array (
         'key' => 'field_533d889dc87b8',
         'label' => 'Meta Description',
@@ -53,6 +63,7 @@
         'readonly' => 0,
         'disabled' => 0,
       ),
+      /* set the advanced tab */
       array (
         'key' => 'field_5342822e6d724',
         'label' => 'Advanced',
@@ -63,6 +74,7 @@
         'required' => 0,
         'conditional_logic' => 0,
       ),
+      /* checkbox to hide page from search engines */
       array (
         'key' => 'field_53428187732fb',
         'label' => 'Private Page',
@@ -76,6 +88,7 @@
         'default_value' => 0,
       ),
     ),
+    /* set the post types to display settings on */
     'location' => array (
       array (
         array (
@@ -91,20 +104,15 @@
           'value' => 'page',
         ),
       ),
-      array (
-        array (
-          'param' => 'post_type',
-          'operator' => '==',
-          'value' => 'snippets',
-        ),
-      ),
+      /* add additional post types here */
     ),
-    'menu_order' => 100,
-    'position' => 'normal',
+    /* field group settings */
+    'menu_order' => 100,                  // importance on edit page - higher numbers are closer to the top
+    'position' => 'normal',               // position on edit page - normal, high, or sidebar
     'style' => 'default',
     'label_placement' => 'top',
     'instruction_placement' => 'field',
     'hide_on_screen' => '',
   ));
-
+  
   endif;
--- seo-sitemap.php
+++ seo-sitemap.php
@@ -1,42 +1,65 @@
 <?php
-
+  
+  /*
+  
+  This file generates our xml sitemap. There is no need
+  do anything with this. It is included within 'seo-functions.php'
+  
+  */ 
+  
+  
   function seo_create_sitemap() {
+    
+    /* Set up the Loop */
     $postsForSitemap = get_posts(array(
-      'numberposts' => -1,
-      'orderby' => 'modified',
-      'post_type'  => array('post','page'),
-      'order'    => 'DESC'
+      'numberposts' => -1,                     // collect all the posts
+      'post_type'  => array('post','page'),    // list the post types to collect
+      'orderby' => 'modified',                 // order by the date modified
+      'order'    => 'DESC'                     // list in descending order
     ));
     
-    $sitemap = '<?xml version="1.0" encoding="UTF-8"?>';
-    $sitemap = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'.
-      '<url>'.
-        '<loc>'. get_bloginfo('wpurl') .'</loc>'.
-        '<priority>1.00</priority>'.
-      '</url>';
-    
-    foreach($postsForSitemap as $post) {
-      setup_postdata($post);
+    /* Write the XML document */
+    $sitemap = '<?xml version="1.0" encoding="UTF-8"?>';                          // set the correct doc type
+    $sitemap = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';    // open the <urlset>
       
-      $postdate = explode(" ", $post->post_modified);
+    if ( is_front_page() && is_home() ) {                 // if Front page == Posts Page write an inital <url> entry for our home page
       
-      $sitemap .= '<url>'.
-        '<loc>'. get_permalink($post->ID) .'</loc>'.
-        '<lastmod>'. $postdate[0] .'</lastmod>'.
-        '<changefreq>monthly</changefreq>'.
-      '</url>';
+      $sitemap = '<url>'.                                 // open the home page <url>
+        '<loc>'. get_bloginfo('wpurl') .'</loc>'.         // pull in the wpurl value as the location
+        '<priority>1.00</priority>'.                      // set as priority 1 - optional
+      '</url>';                                           // close the home page <url>
+      
     }
     
-    $sitemap .= '</urlset>';
+    /* Write the <url> sets for our loop */
+    foreach($postsForSitemap as $post) {                  // run through each of the posts from our loop
+      setup_postdata($post);                              // set up post data so we can access the meta info
+      
+      $postdate = explode(" ", $post->post_modified);     // retrieve the last date the post was modified
+      
+      if( !get_field('private_page', $post->ID) ) {       // if the user has NOT opted to hide the page from Search Engine results
+
+        $sitemap .= '<url>'.                              // open the <url> tag
+          '<loc>'. get_permalink($post->ID) .'</loc>'.    // retrieve the permalink as <loc>
+          '<lastmod>'. $postdate[0] .'</lastmod>'.        // set last modified date - optional
+          '<changefreq>monthly</changefreq>'.             // set <changefreq> - optional
+        '</url>';                                         // close the <url> tag
+        
+      }
+    }
     
-    $fp = fopen(ABSPATH . "sitemap.xml", 'w');
-    fwrite($fp, $sitemap);
-    fclose($fp);
+    $sitemap .= '</urlset>';     // close the <urlset>
+    
+    /* Save the XML document */
+    $saveFile = fopen(ABSPATH . "sitemap.xml", 'w');      // if it exists, open the sitemap
+    fwrite($saveFile, $sitemap);                          // save the sitemap with our generated code
+    fclose($saveFile);                                    // close the sitemap again
   }
   
-  add_action("publish_post", "seo_create_sitemap");
-  add_action("publish_page", "seo_create_sitemap");
-  add_action("trash_post",   "seo_create_sitemap");
-  add_action("trash_page",   "seo_create_sitemap");
-  add_action("untrash_post", "seo_create_sitemap");
-  add_action("untrash_page", "seo_create_sitemap");
+  /* Tell WordPress when to run our function */
+  add_action("publish_post", "seo_create_sitemap");       // on publishing a post
+  add_action("publish_page", "seo_create_sitemap");       // on publishing a page
+  add_action("trash_post",   "seo_create_sitemap");       // on trashing a post
+  add_action("trash_page",   "seo_create_sitemap");       // on trashing a page
+  add_action("untrash_post", "seo_create_sitemap");       // on restoring (untrashing) a post
+  add_action("untrash_page", "seo_create_sitemap");       // on restoring (untrashing) a page
